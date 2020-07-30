7835af25dfc7517af7ea41d0489dc26d353a130b
Zeb Carnell <zebcarnell@gmail.com>
2020-07-30 17:59:35 +1200

    update

--- vm-creation/autounattend/update-win-unattended.sh
+++ vm-creation/autounattend/update-win-unattended.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+git pull origin
 mkdir -p /mnt/fdv/
 mount -o loop win-unattend.vfd /mnt/fdv/
 cp Autounattend.xml /mnt/fdv/
 umount /mnt/fdv/
 git stage *
 git commit -m "Update vfd image with latest autounattend.xml"
+git push
