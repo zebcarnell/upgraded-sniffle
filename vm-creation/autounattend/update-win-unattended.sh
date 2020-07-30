a2187125a75025ccc16c48721af45eb439ffeefe
Zeb Carnell <zebcarnell@gmail.com>
2020-07-30 17:47:56 +1200

    update

--- vm-creation/autounattend/update-win-unattended.sh
+++ vm-creation/autounattend/update-win-unattended.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p /mnt/fdv/
-mount -o loop ~/projects/ansible-playbooks/upgraded-sniffle/vm-creation/autounattend/win-unattend.vfd /mnt/fdv/
-cp ~/projects/ansible-playbooks/upgraded-sniffle/vm-creation/autounattend/Autounattend.xml /mnt/fdv/
+mount -o loop projects/ansible-playbooks/upgraded-sniffle/vm-creation/autounattend/win-unattend.vfd /mnt/fdv/
+cp projects/ansible-playbooks/upgraded-sniffle/vm-creation/autounattend/Autounattend.xml /mnt/fdv/
 umount /mnt/fdv/
 git stage *
 git commit -m "update vfd with latest autounattend.xml"
