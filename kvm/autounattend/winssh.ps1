199ef9291014453a8243c83a4cf3797ee292df93
Zeb Carnell <zebcarnell@gmail.com>
2020-08-06 20:28:57 +1200

    updating openssh install

--- kvm/autounattend/winssh.ps1
+++ kvm/autounattend/winssh.ps1
@@ -1,20 +1,14 @@
 #Requires -Version 3.0
 
-Param (
-        [string]$url = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.1.0.0p1-Beta/OpenSSH-Win64.zip",
-        [string]$output = "C:\OpenSSH\OpenSSH-Win64.zip"
-    )
-
 New-Item -Path "c:\" -Name "OpenSSH" -ItemType "directory" -Force
 
 Start-Transcript -Path C:\OpenSSH\log.txt -Force -noClobber -Append
-Invoke-WebRequest -Uri $url -OutFile $output
-Write-Output "File Downloaded"
-Expand-Archive -LiteralPath C:\OpenSSH\OpenSSH-Win64.zip -DestinationPath C:\OpenSSH\
 
-powershell.exe -ExecutionPolicy Bypass -File C:\OpenSSH\OpenSSH-Win64\install-sshd.ps1
-New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-net start sshd
-Set-Service sshd -StartupType Automatic
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+
+# Get-NetFirewallRule -Name *ssh*
+# New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 
 Stop-Transcript
